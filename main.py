25b470f0c3f0b8068837873b407499b0dead89a7
Mikey <71214259+Master-kyoya@users.noreply.github.com>
2022-08-17 11:07:29 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+print("Hello World!")
+
+# type
+x = 1
+print(x)
+print(type(x))
+
+print("\n********\n")
+
+x = 7.0
+print(x)
+print(type(x))
+
+print("\n********\n")
+
+name = "John Doe"
+print(name)
+print(type(name))
+
+print("\n********\n")
+
+# typecasting
+y = int(x)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+
+print("\n********\n")
+
+z = str(y)
+print(z)
+print(type(z))
+
+print("\n********\n")
+
+# code block
+if x<1:
+ print("yes")
+else:
+ print("no")
+
+print("\n********\n")
+
+# assigning multiple values to several variables
+
+x, y, z = "red", "white", "yellow"
+print(z)
+print(y)
+print(x)
+
+print("\n********\n")
+
+# assigning a value to multiple variables
+x = y = z = 2
+print(x)
+
+print("\n********\n")
+
+# Printing variable values
+name = "John Doe"
+age = 40
+print("Name is: " + name)
+print("Age:" + str(age))
+
+print("\n********\n")
+
+x = "10"
+y = "10"
+print(int(x) + int(y))
+print(type(x))
+
+print("\n********\n")
+
+# IO
+name = input("Enter name: ")
+age = input("Enter age: ")
+
+print("Name: " + name)
+print("Age: " + age)
+print(type(age))
+
+
+print("\n*****END*****\n")
